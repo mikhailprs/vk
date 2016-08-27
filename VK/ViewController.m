fe67aad4e070ff72460e8b931f6ca8577a8a3937
mikhailprs <sponger16@mail.ru>
2016-08-27 14:03:59 +0300

    Initial Commit

---
+++ VK/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  VK
+//
+//  Created by Mikhail Prysiazhniy on 27.08.16.
+//  Copyright © 2016 Mikhail Prysiazhniy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ VK/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  VK
+//
+//  Created by Mikhail Prysiazhniy on 27.08.16.
+//  Copyright © 2016 Mikhail Prysiazhniy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
